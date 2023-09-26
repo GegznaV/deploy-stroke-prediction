42d7c14a1e55d824bc37619d6f42e06fd24400fb
Vilmantas Gėgžna <GegznaV@gmail.com>
2023-09-26 10:16:46 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 """Functions and classes used in the project."""
 import numpy as np
+import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
 def get_stroke_risk_trend(age, base_prob=1, age_threshold=40):
